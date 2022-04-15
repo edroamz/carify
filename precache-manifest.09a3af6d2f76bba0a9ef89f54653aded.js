1f2f791377a9be0497b2c78f140e59eea4d30e39
Edroamz <35645733+edroamz@users.noreply.github.com>
2022-04-15 00:19:10 -0500

    Updates

---
+++ precache-manifest.09a3af6d2f76bba0a9ef89f54653aded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f987cec90f54caf848e3b48ddf9c4b4c",
+    "url": "/carlify/index.html"
+  },
+  {
+    "revision": "4e090664ab54f5f79b65",
+    "url": "/carlify/static/css/main.895860f5.chunk.css"
+  },
+  {
+    "revision": "e2941799f4db8ffc4f08",
+    "url": "/carlify/static/js/2.a9b71f8e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/carlify/static/js/2.a9b71f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e090664ab54f5f79b65",
+    "url": "/carlify/static/js/main.d12ed451.chunk.js"
+  },
+  {
+    "revision": "66932a657358800ce5e1",
+    "url": "/carlify/static/js/runtime-main.36edfe13.js"
+  },
+  {
+    "revision": "8a0570ed94a9c68744c83378964082f9",
+    "url": "/carlify/static/media/hero.8a0570ed.jpg"
+  }
+]);
